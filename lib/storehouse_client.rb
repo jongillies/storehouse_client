76fdad33870282ba621117585ed093f01deb38d4
Jon Gillies <jon@gillies.us>
2013-08-11 12:30:36 -0700

    minor reformatting of code.  added a begin rescue around add_Record to return any encoding or API errors.

--- lib/storehouse_client.rb
+++ lib/storehouse_client.rb
@@ -16,12 +16,13 @@ module StorehouseClient
 
     def initialize(args)
 
-      @error = nil
-      @url = args.fetch(:url) { raise 'You must provide :url' }
+      @error       = nil
+      @url         = args.fetch(:url) { raise 'You must provide :url' }
       @api_version = args.fetch(:api_version) { 1 }
-      @auth_token = args.fetch(:auth_token) { raise 'You must provide :auth_token' }
+      @auth_token  = args.fetch(:auth_token) { raise 'You must provide :auth_token' }
+
       @record_count = 0
-      @duration = nil
+      @duration     = nil
 
       @resource = RestClient::Resource.new @url
 
@@ -39,10 +40,10 @@ module StorehouseClient
 
       @data_source_id = data_source_id
 
-      run = {}
-      run['export_run'] = {}
+      run                                 = {}
+      run['export_run']                   = {}
       run['export_run']['data_source_id'] = @data_source_id
-      run['export_run']['started_at'] = Time.now
+      run['export_run']['started_at']     = Time.now
 
       r = post("api/#{@api_version}/export_runs?auth_token=#{@auth_token}", run)
 
@@ -55,9 +56,9 @@ module StorehouseClient
 
     def finish_run
 
-      run = {}
-      run['export_run'] = {}
-      run['export_run']['finished_at'] = Time.now.to_s
+      run                               = {}
+      run['export_run']                 = {}
+      run['export_run']['finished_at']  = Time.now.to_s
       run['export_run']['record_count'] = @record_count
 
       r = put("api/#{@api_version}/export_runs/#{@run_id}?auth_token=#{@auth_token}", run)
@@ -71,22 +72,23 @@ module StorehouseClient
 
     def add_record(primary_key, data)
 
-      # primary_key must be a string
+      begin
+        e                                                 = {}
+        e['export_record']                                = {}
+        e['export_record']['blob_attributes']             = {}
+        e['export_record']['blob_attributes']['data']     = data
+        e['export_record']['blob_attributes']['checksum'] = Digest::SHA256.hexdigest(data.to_json)
+        e['export_record']['record_size']                 = data.to_json.length
+        e['export_record']['checksum']                    = e['export_record']['blob_attributes']['checksum']
+        e['export_record']['primary_key']                 = primary_key.to_s # Ensure primary key is a string
+        e['export_record']['data_source_id']              = @data_source_id
+        e['export_record']['export_run_id']               = @run_id
 
-      e = {}
-      e['export_record'] = {}
-      e['export_record']['blob_attributes'] = {}
-      e['export_record']['blob_attributes']['data'] = data
-      e['export_record']['blob_attributes']['checksum'] = Digest::SHA256.hexdigest(data.to_json)
-      e['export_record']['record_size'] = data.to_json.length
-      e['export_record']['checksum'] = e['export_record']['blob_attributes']['checksum']
-      e['export_record']['primary_key'] = primary_key.to_s # Ensure primary key is a string
-      e['export_record']['data_source_id'] = @data_source_id
-      e['export_record']['export_run_id'] = @run_id
+        post("api/#{@api_version}/export_records/?auth_token=#{@auth_token}", e)
 
-      post("api/#{@api_version}/export_records/?auth_token=#{@auth_token}", e)
-
-      return nil if error?
+      rescue Exception => @error
+        return nil
+      end
 
       @record_count += 1
 
